fe859bb52e9d5b9d365e1acaf44450a6818b511d
Pattjjeee <79310750+Pattjjeee@users.noreply.github.com>
2021-02-28 23:06:39 +0100

    Eerste kale versie
    Eerste mobiele styling gedaan en testgrafiek toegevoegd

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+var ctx = document.getElementById('chart-1').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['2013', '2014', '2015', '2015', '2016', '2017'],
+        datasets: [{
+            label: 'Tesla stock 2013-2017',
+            backgroundColor: '#48839d',
+            data: [15, 20, 25, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var ctx = document.getElementById('chart-2').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['2013', '2014', '2015', '2015', '2016', '2017'],
+        datasets: [{
+            label: 'Tesla stock 2013-2017',
+            backgroundColor: '#48839d',
+            data: [15, 20, 25, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var ctx = document.getElementById('chart-3').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['2013', '2014', '2015', '2015', '2016', '2017'],
+        datasets: [{
+            label: 'Tesla stock 2013-2017',
+            backgroundColor: '#48839d',
+            data: [15, 20, 25, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
