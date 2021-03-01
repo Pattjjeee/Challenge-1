de4ead713b3d116df56d92e1acf5a125f34bebe4
Pattjjeee <79310750+Pattjjeee@users.noreply.github.com>
2021-03-01 14:41:54 +0100

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Grafiek 1
+
 var ctx = document.getElementById('chart-1').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -17,40 +19,29 @@ var chart = new Chart(ctx, {
     options: {}
 });
 
-var ctx = document.getElementById('chart-2').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
+// Grafiek 2
 
-    // The data for our dataset
+var ctx = document.getElementById('donut').getContext('2d');
+var chart = new Chart(ctx, {
+   
+    type: 'doughnut',
+
     data: {
-        labels: ['2013', '2014', '2015', '2015', '2016', '2017'],
+        labels: ['Aantal ruimte maaltijden', 'Flessen ruimte sap', 'Ruimte noodles'],
         datasets: [{
-            label: 'Tesla stock 2013-2017',
-            backgroundColor: '#48839d',
-            data: [15, 20, 25, 20, 30, 45]
+            label: '',
+            data: [1820, 1430, 450],
+            backgroundColor: [
+            '#48839d',
+            '#204553',
+            ],
         }]
     },
 
-    // Configuration options go here
-    options: {}
-});
+   
+    options: {
 
-var ctx = document.getElementById('chart-3').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: ['2013', '2014', '2015', '2015', '2016', '2017'],
-        datasets: [{
-            label: 'Tesla stock 2013-2017',
-            backgroundColor: '#48839d',
-            data: [15, 20, 25, 20, 30, 45]
-        }]
     },
-
-    // Configuration options go here
-    options: {}
+    }
 });
+
